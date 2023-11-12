adc1dd6759e63be0097ff50bce7f655f502d5e72
merta <merta@10.57.51.165>
2023-11-12 13:06:31 +0300

    23010310010_MertAşkan_Hafta6_Soru2.java

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 0;
+		int toplam = 0,gecici;
+		
+		while(sayi <=100) {
+			gecici = sayi * sayi;
+			toplam += gecici;
+			sayi++;
+			System.out.println(toplam);
+			
+					
+		}
+
+	}
+
+}
